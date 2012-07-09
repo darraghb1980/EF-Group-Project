8749598788ec2512fa0af79a5b39582e3cf40883
Fintan Costello <fintanc@gmail.com>
2012-07-09 16:25:49 +0100

    Data normalisation

--- teamcanada/teamcanada/ingestion/IgetCSV.cs
+++ teamcanada/teamcanada/ingestion/IgetCSV.cs
@@ -14,7 +14,7 @@ namespace teamcanada.ingestion
         public List<Election> loadElection()
         {
             List<Election> lst = new List<Election>();
-            StreamReader reader = new StreamReader("C:\\EF Project\\EF-Group-Project\\datasets\\election_results\\2006\\Poll_by_Poll_2006-Council.csv", true);
+            StreamReader reader = new StreamReader("C:\\EF Project\\EF-Group-Project\\datasets\\results_mayoral.csv", true);
             abc.setStreamSource(reader);
             lst = abc.parseElection();
             return lst;
