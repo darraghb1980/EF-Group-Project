67dd00ea241088ff7f1c2a1eb3350f1647558a67
Fintan Costello <fintanc@gmail.com>
2012-07-09 21:45:42 +0100

    Parser code changed

--- teamcanada/teamcanada/ingestion/ResultsContribParse.cs
+++ teamcanada/teamcanada/ingestion/ResultsContribParse.cs
@@ -94,17 +94,53 @@ namespace teamcanada.ingestion
                     {
                         exObj.ElectionYear = Convert.ToInt32(csv[i]);
                     }
+                    else if (headers[i].Equals("CONTRIBUTORFIRSTNAME"))
+                    {
+                        exObj.ContributorFirstName = csv[i];
+                    }
+                    else if (headers[i].Equals("CONTRIBUTORLASTNAME"))
+                    {
+                        exObj.ContributorLastName = csv[i];
+                    }
+                    else if (headers[i].Equals("CONTRIBUTORPOSTCODE"))
+                    {
+                        exObj.ContributorPostCode = csv[i];
+                    }
+                    else if (headers[i].Equals("CONTRIBUTORADDRESS"))
+                    {
+                        exObj.ContributorAddress = csv[i];
+                    }
                     else if (headers[i].Equals("WARDNUM"))
                     {
                         exObj.WardNum = Convert.ToInt32(csv[i]);
                     }
-                    else if (headers[i].Equals("CANDIDATELASTNAME"))
+                    else if (headers[i].Equals("AMOUNT"))
                     {
-                        exObj.CandidateLastName = csv[i];
+                        exObj.Amount = Convert.ToSingle(csv[i]);
+                    }
+                    else if (headers[i].Equals("CONTRIBUTIONTYPE"))
+                    {
+                        exObj.ContributionType = csv[i];
+                    }
+                    else if (headers[i].Equals("CONTRIBUTIONTYPEDESC"))
+                    {
+                        exObj.ContributionTypeDesc = csv[i];
+                    }
+                    else if (headers[i].Equals("CONTRIBUTORTYPE"))
+                    {
+                        exObj.ContributorType = csv[i];
+                    }
+                    else if (headers[i].Equals("CONTRIBUTORTYPEDESC"))
+                    {
+                        exObj.ContributorTypeDesc = csv[i];
                     }
                     else if (headers[i].Equals("CANDIDATEFIRSTNAME"))
                     {
                         exObj.CandidateFirstName = csv[i];
+                    }
+                    else if (headers[i].Equals("CANDIDATELASTNAME"))
+                    {
+                        exObj.CandidateLastName = csv[i];
                     }
 
                 }
