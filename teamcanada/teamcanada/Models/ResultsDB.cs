dbba62ca4430a92594077a771d847995ebe9e1a1
Fintan Costello <fintanc@gmail.com>
2012-07-11 17:22:52 +0100

    testing DB access

---
+++ teamcanada/teamcanada/Models/ResultsDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace teamcanada.Models
+{
+    public class ResultsDb : DbContext
+
+    {
+        public DbSet<Results> results { get; set; }
+    }
+}
---
+++ teamcanada/teamcanada/dataaccess/ResultsDAL.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using teamcanada.Models;
+using System.Configuration;
+
+namespace teamcanada.dal
+{
+    public class ResultsDAL
+    {
+        protected ResultsDb db = new ResultsDb();
+
+        public ResultsDAL()
+        {
+            ConnectionStringSettingsCollection connections = ConfigurationManager.ConnectionStrings;
+            if (connections.Count != 0)
+            {
+                Console.WriteLine();
+                System.Diagnostics.Debug.WriteLine("Using ConnectionStrings property");
+
+                //get the collection elements
+                foreach (ConnectionStringSettings connection in connections)
+                {
+                    string name = connection.Name;
+                    string provider = connection.ProviderName;
+                    string connectionString = connection.ConnectionString;
+
+                    System.Diagnostics.Debug.WriteLine("Name:       {0}", name);
+                    System.Diagnostics.Debug.WriteLine("Connection String:       {0}", connectionString);
+                    System.Diagnostics.Debug.WriteLine("Provider:       {0}", provider);
+
+                }
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("no connection string is defined");
+            }
+
+
+        }
+
+        public ExchangeRate findExchangeRateForConversion(String from, String to)
+        {
+            List<ExchangeRate> rates = null;
+            rates = db.exchangeRates.ToList();
+
+            foreach (ExchangeRate r in rates)
+            {
+                if (r.toCurrency.Equals(to) && r.fromCurrency.Equals(from))
+                {
+                    return r;
+                }
+            }
+            return null;
+        }
+
+        /*      public ExchangeRate findExchangeRateById(int id) {
+                  return null;
+              }
+
+              public ExchangeRate addExchangeRate(ExchangeRate rate)
+              {
+                  return null;
+              }*/
+    }
+}
