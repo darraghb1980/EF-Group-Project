bf6109e99474cca62a887032212a3268e6b4526b
Alan McCormack <almccormack@gmail.com>
2012-07-18 14:16:02 +0100

    First attempt at business logic
    Added separate folder 'Logic' that will contain all business logic.

---
+++ teamcanada/teamcanada/Logic/CalculateTotalVotes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace teamcanada.Logic
+{
+    public class CalculateTotalVotes
+    {
+        Results totalResult = new Results();
+        viewDAL dbSelectVotes = new viewDAL();
+        
+        int totalVotes = 0;
+        int rowVoteCount
+
+        for (int i = 0; int <= rowCount; i++)                      // for loop
+        {
+            totalVotes = totalVotes + totalResult.NumVotes;       // keep adding amounts into totalAmount
+        }
+
+        return totalVotes;
+    }
+}
